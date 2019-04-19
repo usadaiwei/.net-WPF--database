e667edbc19b924c7ec19ab05faa632408fe31ae1
usadaiwei <usadaiwei@gmail.com>
2019-04-18 23:36:58 -0400

    Add files via upload

---
+++ Demo/Demo/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+
+namespace Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            String name = lastName+" "+firstName;
+            //Do validation first
+            if (firstName.Text == ""|| lastName.Text ==""||birthday.Text=="")
+                MessageBox.Show("Please enter your information!");
+            //calculate age
+            
+            int year = 0;
+            DateTime oDate = Convert.ToDateTime(birthday.Text);
+            year = DateTime.Now.Year - oDate.Year;
+            if (DateTime.Now.DayOfYear < year)
+                year -= 1;
+            String age = year.ToString();
+
+            //connect database
+            SqlConnection conn = new SqlConnection("Data Source=(local); Initial Catalog=company; Integrated Security=True");
+            conn.Open();
+
+            //insert user data
+            string str = "insert into Employee(firstName, lastName, birthday, age) values('" + firstName.Text + "','" + lastName.Text + "','" + birthday.Text + "','" + age + "')";
+
+            SqlCommand insert = new SqlCommand(str, conn);
+
+            insert.ExecuteNonQuery();
+            conn.Close();
+            conn.Dispose();
+            //go to hello page
+            hello newWindow = new hello(name, age);
+            //newWindow.Title = "Thank you!";
+            //newWindow.Content = "Thank you, " + name + ", " + age + " years old";
+            newWindow.Show();
+            this.Close();
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ Demo/Demo/hello.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Demo
+{
+    /// <summary>
+    /// Interaction logic for hello.xaml
+    /// </summary>
+    public partial class hello : Window
+    {
+        String _name;
+        String _age;
+        public hello(String name, String age)
+        {
+            InitializeComponent();
+            _name = name;
+            _age = age;
+        }
+
+        private void Window2_Loaded(object sender, RoutedEventArgs e)
+        {
+            String userName = _name;
+            String userAge = _age;
+            //String output = "Thank you, " + userName + ", " + userAge + " years old";
+            //var label = sender as Label;
+            //label.Content = output;
+            aa.Content = userName;
+            bb.Content = userAge;
+            //MessageBox.Show(output);
+
+        }
+    }
+}
